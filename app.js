7e5f71ee3a9ad1774d5b7d62990c3d5848071105
David.Inacio <david.inacio.rosa@gmail.com>
2020-07-04 20:49:42 -0300

    Commit Inicial

---
+++ app.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { studentRoutes } from './routes/studentRoutes.js';
+
+const app = express();
+
+//Conectar ao MongoDB pelo Mongoose
+(async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://sa:Master@2018@clusterigtimongodb.wtcxg.gcp.mongodb.net/grades?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+  } catch (error) {
+    console.log('Erro ao conectar no MongoDB: ' + error);
+  }
+})();
+
+/*Conexao com o MongoDB*/
+// (async () => {
+//   try {
+//     await mongoose.connect(
+//       'mongodb+srv://' +
+//         process.env.USERDB +
+//         ':' +
+//         process.env.PWDDB +
+//         '@clusterigtimongodb.wtcxg.gcp.mongodb.net/grades?retryWrites=true&w=majority',
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       }
+//     );
+//   } catch (error) {
+//     console.log('Erro ao conectar no MongoDB: ' + error);
+//   }
+// })();
+
+app.use(express.json());
+app.use(studentRoutes);
+
+// app.listen(process.env.PORT, () => console.log('Servidor em execucao'));
+app.listen(3000, () => console.log('API Iniciada'));
---
+++ routes/studentRoutes.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import { studentModel } from '../models/studentModel.js';
+
+const app = express();
+
+//validação da rota
+// app.get('/student', async (req, res) => {
+//   res.send({ result: 'validado!' });
+// });
+
+//retrielve
+app.get('/student', async (req, res) => {
+  const student = await studentModel.find({});
+  try {
+    res.send(student);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+//create
+app.post('/student', async (req, res) => {
+  const student = new studentModel(req.body);
+  try {
+    await student.save();
+    res.send(student);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+//delete
+app.delete('/student/:id', async (req, res) => {
+  try {
+    const student = await studentModel.findOneAndDelete({ _id: req.params.id });
+    if (!student) {
+      res.status(404).send('Documento não encontrado');
+    }
+    res.status(200).send();
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.patch('/student/:id', async (req, res) => {
+  try {
+    const student = await studentModel.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+    res.send(student);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+export { app as studentRoutes };
